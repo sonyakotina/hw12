2b2799ccc463107567d04e143e68b22b47b55d9f
sonyakotina <sonyakotina@gmail.com>
2022-05-20 22:14:30 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include "main.h"
+
+int main()
+{
+    int dsize = 0;
+    cout << "Type double array size: ";
+    if (!(cin >> dsize))
+    {
+        cout << "Invalid input!";
+        return 1;
+    }
+    double* darr = new double[dsize];
+    cout << "Filling double array: ";
+    fillArr(darr, dsize);
+
+    int isize = 0;
+    cout << "Type int array size: ";
+    if (!(cin >> isize))
+    {
+        cout << "Invalid input!";
+        return 1;
+    }
+    int* iarr = new int[isize];
+    cout << "Filling int array: ";
+    fillArr(iarr, isize);
+
+    cout << "Unsorted int array: " << endl;
+    printArr(iarr, isize);
+    selectionSort(iarr, isize);
+    cout << "Sorted int array: " << endl;
+    printArr(iarr, isize);
+
+    cout << "Unsorted double array: " << endl;
+    printArr(darr, dsize);
+    selectionSort(darr, dsize);
+    cout << "Sorted double array: " << endl;
+    printArr(darr, dsize);
+}
+
+template <typename T>
+void selectionSort(T* arr, int size) {
+    for (int i = 0; i < size; i++)
+    {
+        int key = i;
+        double mini = arr[i];
+        for (int j = i; j < size; j++)
+        {
+            if (arr[j] < mini)
+            {
+                mini = arr[j];
+                key = j;
+            }
+        }
+        if (key != i)
+        {
+            swap(arr[i], arr[key]);
+        }
+    }
+}
+
+template <typename T>
+void fillArr(T* arr, int size) {
+    for (int i = 0; i < size; i++)
+    {
+        if (!(cin >> arr[i]))
+        {
+            i--;
+            continue;
+        }
+    }
+}
+
+template <typename T>
+void printArr(T* arr, int size) {
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void selectionSort(T* arr, int size);
+
+template <typename T>
+void fillArr(T* arr, int size);
+
+template <typename T>
+void printArr(T* arr, int size);
+
+#endif
